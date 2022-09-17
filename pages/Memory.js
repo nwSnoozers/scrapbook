f5d4e24e0f10a785db96e93d29e32fcf96bf63f3
Taryn Wou <75547547+tarynwou@users.noreply.github.com>
2022-09-17 04:37:48 -0400

    image upload functionality

--- pages/Memory.js
+++ pages/Memory.js
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { SafeAreaView, TextInput } from "react-native";
+import { StyleSheet, Text, View, TextInput } from "react-native";
+
+// date picker import
+import DatePicker from "react-native-date-picker";
+
+// photo upload imports
+import { Image, Platform, TouchableOpacity } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+import * as ImagePicker from "expo-image-picker";
 
 const MemoryPage = () => {
+	// text input consts
 	const [note, onChangeNote] = React.useState("Add your notes here");
 	const [location, onChangeLocation] = React.useState("Where did you go?");
-	const [number, onChangeNumber] = React.useState(null);
+
+	// image upload consts
+	const [image, setImage] = useState(null);
+
+	const addImage = async () => {
+		let _image = await ImagePicker.launchImageLibraryAsync({
+			mediaTypes: ImagePicker.MediaTypeOptions.Images,
+			allowsEditing: true,
+			aspect: [4, 3],
+			quality: 1,
+		});
+		console.log(JSON.stringify(_image));
+		if (!_image.cancelled) {
+			setImage(_image.uri);
+		}
+	};
+
 	return (
 		<View
 			style={{
@@ -25,6 +49,24 @@ const MemoryPage = () => {
 			/>
 			<Text>Personal rating</Text>
 			<Text>Photo</Text>
+			<View style={imageUploaderStyles.container}>
+				{image && (
+					<Image
+						source={{ uri: image }}
+						style={{ width: 200, height: 200 }}
+					/>
+				)}
+				<View style={imageUploaderStyles.uploadBtnContainer}>
+					<TouchableOpacity
+						onPress={addImage}
+						style={imageUploaderStyles.uploadBtn}
+					>
+						<Text>{image ? "Edit" : "Upload"} Image</Text>
+						<AntDesign name="camera" size={20} color="black" />
+					</TouchableOpacity>
+				</View>
+			</View>
+
 			<Text>Notes Section</Text>
 			<TextInput
 				style={styles.notesInput}
@@ -35,6 +77,31 @@ const MemoryPage = () => {
 		</View>
 	);
 };
+
+const imageUploaderStyles = StyleSheet.create({
+	container: {
+		elevation: 2,
+		height: 200,
+		width: 200,
+		backgroundColor: "#efefef",
+		position: "relative",
+		overflow: "hidden",
+	},
+	uploadBtnContainer: {
+		opacity: 0.7,
+		position: "absolute",
+		right: 0,
+		bottom: 0,
+		backgroundColor: "lightgrey",
+		width: "100%",
+		height: "25%",
+	},
+	uploadBtn: {
+		display: "flex",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+});
 
 const styles = StyleSheet.create({
 	locationInput: {
